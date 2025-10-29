909c83dae9ed17b2e5d04e2fdcde60dd50684c6c
Bukaro <jalexisg@gmail.com>
2025-10-29 16:30:01 +0000

    Printing Salary Information

--- src/main/java/com/napier/sem/Employee.java
+++ src/main/java/com/napier/sem/Employee.java
@@ -49,6 +49,18 @@ public class Employee
         {
             System.err.println("Failed to get salary details: " + e.getMessage());
         }
+
+        // Print header
+        System.out.println(String.format("%-10s %-15s %-20s %-8s", "Emp No", "First Name", "Last Name", "Salary"));
+        // Loop over all employees in the list and print
+        for (Employee emp : employees)
+        {
+            String emp_string =
+                    String.format("%-10s %-15s %-20s %-8s",
+                            emp.emp_no, emp.first_name, emp.last_name, emp.salary);
+            System.out.println(emp_string);
+        }
+
         return employees;
     }
 }
